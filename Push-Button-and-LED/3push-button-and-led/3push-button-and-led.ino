0180dda2263f62aec7f33628edbaa647a08012ae
Douglas Deodato <douglasdeodato@gmail.com>
2020-02-06 17:52:30 +0000

    some code improvement

--- Push-Button-and-LED/3push-button-and-led/3push-button-and-led.ino
+++ Push-Button-and-LED/3push-button-and-led/3push-button-and-led.ino
@@ -1,29 +1,40 @@
+const uint8_t   LED_OFF         =  LOW;     
+const uint8_t   LED_ON          = HIGH;    
+
+const uint8_t   pinGREEN_BUTTON       = 2;
+const uint8_t   pinRED_BUTTON         = 3;
+const uint8_t   pinYELLOW_BUTTON      = 4;
+
+const uint8_t   pinGREEN_LED          = 10;
+const uint8_t   pinRED_LED            = 9;
+const uint8_t   pinYELLOW_LED         = 8;
+
 void setup()
 
 {
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(pinGREEN_BUTTON, INPUT_PULLUP);
+  pinMode(pinRED_BUTTON, INPUT_PULLUP);
+  pinMode(pinYELLOW_BUTTON, INPUT_PULLUP);
+  pinMode(pinGREEN_LED, OUTPUT);
+  pinMode(pinRED_LED, OUTPUT);
+  pinMode(pinYELLOW_LED, OUTPUT);
 }
 
 void loop()
 {
-  if (digitalRead(2) == LOW) {
-    digitalWrite(10, HIGH);
+  if (digitalRead(pinGREEN_BUTTON) == LED_OFF){
+    digitalWrite(pinGREEN_LED, LED_ON);
   }
-  else if (digitalRead(3) == LOW) {
-    digitalWrite(9, HIGH);
+  else if (digitalRead(pinRED_BUTTON) == LED_OFF) {
+    digitalWrite(pinRED_LED, LED_ON);
   }
-   else if (digitalRead(4) == LOW) {
-    digitalWrite(8, HIGH);
+   else if (digitalRead(pinYELLOW_BUTTON) == LED_OFF) {
+    digitalWrite(pinYELLOW_LED, LED_ON);
   }
   else {
     // turn off LED.
-    digitalWrite(10, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(8, LOW);
+    digitalWrite(pinGREEN_LED,LED_OFF);
+    digitalWrite(pinRED_LED, LED_OFF);
+    digitalWrite(pinYELLOW_LED, LED_OFF);
   }
 }
